060b4dcbddbc4a08cf3a0b8b1291d5906c80726e
kmedhurs <kmedhurs@student.21-school.ru>
2019-01-31 19:07:18 +0300

    g

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: ffahey <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 19:15:38 by ffahey            #+#    #+#             */
-/*   Updated: 2019/01/30 19:37:05 by marvin           ###   ########.fr       */
+/*   Updated: 2019/01/31 18:40:26 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,43 @@
 
 # include <stdio.h>
 # include <stdlib.h>
-# include "libft.h"
 
 # define OUT_OF_MEMORY -1
 
 # define START	-1
 # define END	-2
 
-# define ROOM	farm->room->name
+# define ROOM	farm->rooms
 # define ANT(i)	farm->ants[i]
 # define LINKS	farm->links
 
 typedef struct		s_room
 {
-	char	*name;
-	t_room	*links;
-	int		x;
-	int		y;
-	char	state;//START, END or ant number
-	t_room	*next;
+	char			*name;
+	int				id;
+	struct s_room	*links;
+	int				x;
+	int				y;
+	char			state;//START, END or ant number
+	struct s_room	*next;
 }					t_room;
 
 typedef struct		s_farm
 {
-	size_t	rooms_count;
-	t_room	*rooms;
-	size_t	ants_count;
-	char	**ants;
+	size_t			rooms_count;
+	t_room			*rooms;
+	size_t			ants_count;
+	char			**ants;
 }					t_farm;
 
 //ffahey part
 
 //kmedhurs part
+
+t_farm				*ft_init_farm(void);
+void				ft_print_farm(t_farm *farm);
+t_room				*ft_init_room(t_room *room, char *name, char state, int id);
+t_room				*ft_search_node(t_room *room, int id);
+t_path				*ft_complete_path(t_path *path, t_room *ptr);
 
 #endif
--- srcs/delete_this_shit.c
+++ srcs/delete_this_shit.c
@@ -6,11 +6,23 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 18:52:38 by marvin            #+#    #+#             */
-/*   Updated: 2019/01/30 19:36:03 by marvin           ###   ########.fr       */
+/*   Updated: 2019/01/31 18:35:11 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem-in.h"
+#include <stdio.h>
+
+t_room	*ft_search_node(t_room *room, int id)
+{
+	while (room)
+	{
+		if (room->id == id)
+			return (room);
+		room = room->next;
+	}
+	return (NULL);
+}
 
 t_room	*ft_init_room(t_room *room, char *name, char state, int id)
 {
@@ -19,6 +31,7 @@ t_room	*ft_init_room(t_room *room, char *name, char state, int id)
 	new_room = (t_room*)malloc(sizeof(t_room));
 	room->name = name;
 	room->id = id;
+	room->links = NULL;
 	room->x = 0;
 	room->y = 0;
 	room->state = state;
@@ -35,6 +48,7 @@ t_farm	*ft_init_farm(void)
 	new_farm->rooms_count = 8;
 	new_farm->rooms = NULL;
 	new_farm->rooms = ft_init_room(new_farm->rooms, "Start", START, 0);
+	new_farm->rooms->links = NULL;
 	new_farm->rooms = ft_init_room(new_farm->rooms, "kmedhurs", 0, 1);
 	new_farm->rooms = ft_init_room(new_farm->rooms, "ffahey", 0, 2);
 	new_farm->rooms = ft_init_room(new_farm->rooms, "trhogoro", 0, 3);
@@ -44,4 +58,20 @@ t_farm	*ft_init_farm(void)
 	new_farm->rooms = ft_init_room(new_farm->rooms, "Finish", END, 7);
 	new_farm->ants_count = 4;
 	new_farm->ants = NULL;
+}
+
+void	ft_print_farm(t_farm *farm)
+{
+	int		i;
+
+	i = -1;
+	while (++i < farm->rooms_count)
+	{
+		printf("Room: %d, Name: %s", farm->rooms->id, farm->rooms->name);
+		while (farm->rooms->links)
+		{
+			printf(" Ptr: %d", farm->rooms->links->ptr->id);
+			farm->rooms->links = farm->rooms->links->next;
+		}
+	}
 }
